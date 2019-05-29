c531137849522aa170340e52a966c8dc0c6bae50
Victor Martinez <knoopx@gmail.com>
2019-05-29 15:33:52 +0200

    wip

--- spec/refactorings.spec.js
+++ spec/refactorings.spec.js
@@ -2,38 +2,43 @@ import {
   parse,
   generate,
   getPathAtPosition,
-  renameVariable,
-  renameFunction,
-  extractVariableIntoModule,
-  extractClassIntoModule,
-  deanonymifyClassDeclaration
-} from "../lib/refactoring";
+} from "../lib/helpers"
+
+// import {
+//   renameVariable,
+//   renameFunction,
+//   extractVariableIntoModule,
+//   extractClassIntoModule,
+//   deanonymifyClassDeclaration,
+//   statefulToStatelessComponent
+// } from '../lib/index'
+
+import statefulToStatelessComponent from "../lib/statefulToStateless"
+import extractClassIntoModule from "../lib/extractClassIntoModule"
 
 describe("refactorings", () => {
-  it("extractVariableIntoModule", async function(done) {
-    const code = `const xxx = 1`;
+  it("extractVariableIntoModule", async (done) => {
+    const code = "const xxx = 1"
 
-    const ast = parse(code);
+    const ast = parse(code)
 
     const moduleAst = await extractVariableIntoModule(
       ast,
       {
         row: 0,
-        column: 8
+        column: 8,
       },
-      function(name) {
-        return "./" + name + ".js";
-      }
-    );
+      (name) => `./${name}.js`,
+    )
 
-    expect(generate(ast, code)).toEqual('import xxx from "./xxx.js";\n');
+    expect(generate(ast, code)).toEqual('import xxx from "./xxx.js";\n')
     expect(generate(moduleAst, code)).toEqual(
-      "const xxx = 1;\nexport default xxx;\n"
-    );
-    done();
-  });
+      "const xxx = 1;\nexport default xxx;\n",
+    )
+    done()
+  })
 
-  it("extractClassIntoModule", async function(done) {
+  it("extractClassIntoModule", async (done) => {
     const code = `import React from 'react'
 class X extends React.PureComponent {
   render() {
@@ -44,25 +49,23 @@ class X extends React.PureComponent {
 }
 
 export default () => <Y/>
-`;
+`
 
-    const ast = parse(code);
+    const ast = parse(code)
 
     const moduleAst = await extractClassIntoModule(
       ast,
       {
         row: 1,
-        column: 7
+        column: 7,
       },
-      function(name) {
-        return "./" + name + ".js";
-      }
-    );
+      (name) => `./${name}.js`,
+    )
 
     expect(generate(ast, code)).toEqual(`import X from \"./X.js\";
 
 export default () => <Y />;
-`);
+`)
     expect(generate(moduleAst, code)).toEqual(`import React from \"react\";
 class X extends React.PureComponent {
   render() {
@@ -70,47 +73,60 @@ class X extends React.PureComponent {
   }
 }
 export default X;
-`);
-    done();
-  });
+`)
+    done()
+  })
 
   describe("deanonymifyClassDeclaration", () => {
     it("deanonyfies class", () => {
-      const code = `const X = class {}`;
-      const ast = parse(code);
-      const path = getPathAtPosition(ast, { row: 0, column: 7 });
-      deanonymifyClassDeclaration(path);
-      expect(generate(ast, code)).toEqual("class X {}\n");
-    });
-  });
+      const code = "const X = class {}"
+      const ast = parse(code)
+      const path = getPathAtPosition(ast, { row: 0, column: 7 })
+
+      deanonymifyClassDeclaration(path)
+      expect(generate(ast, code)).toEqual("class X {}\n")
+    })
+  })
 
   describe("renameVariable", () => {
-    it("renames variable declarations", function() {
-      const code = `const xxx = 1`;
-      const ast = parse(code);
-      const path = getPathAtPosition(ast, { row: 0, column: 8 });
-      renameVariable(path, "yyy");
-      expect(generate(ast, code)).toEqual("const yyy = 1;\n");
-    });
+    it("renames variable declarations", () => {
+      const code = "const xxx = 1"
+      const ast = parse(code)
+      const path = getPathAtPosition(ast, { row: 0, column: 8 })
 
-    it("renames local variable declarations", function() {
-      const code = `function func(xxx) { return xxx }`;
-      const ast = parse(code);
-      const path = getPathAtPosition(ast, { row: 0, column: 15 });
-      renameVariable(path, "yyy");
+      renameVariable(path, "yyy")
+      expect(generate(ast, code)).toEqual("const yyy = 1;\n")
+    })
+
+    it("renames local variable declarations", () => {
+      const code = "function func(xxx) { return xxx }"
+      const ast = parse(code)
+      const path = getPathAtPosition(ast, { row: 0, column: 15 })
+
+      renameVariable(path, "yyy")
       expect(generate(ast, code)).toEqual(
-        `function func(yyy) {\n  return yyy;\n}\n`
-      );
-    });
-  });
+        "function func(yyy) {\n  return yyy;\n}\n",
+      )
+    })
+  })
 
   describe("renameFunction", () => {
-    it("renames function declarations", function() {
-      const code = `function xxx() {}; xxx();`;
-      const ast = parse(code);
-      const path = getPathAtPosition(ast, { row: 0, column: 8 });
-      renameFunction(path, "yyy");
-      expect(generate(ast, code)).toEqual("function yyy() {}\nyyy();\n");
-    });
-  });
-});
+    it("renames function declarations", () => {
+      const code = "function xxx() {}; xxx();"
+      const ast = parse(code)
+      const path = getPathAtPosition(ast, { row: 0, column: 8 })
+
+      renameFunction(path, "yyy")
+      expect(generate(ast, code)).toEqual("function yyy() {}\nyyy();\n")
+    })
+  })
+
+  describe("statefulToStatelessComponent", () => {
+    it("transform statefulToStateless components", () => {
+      const code = "class X extends React.Component {}"
+      const ast = parse(code)
+
+      statefulToStatelessComponent()
+    })
+  })
+})
